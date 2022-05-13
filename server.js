66e5890d25e048d6803f95b7c90eee5dcefa2f76
Sashi445 <sashidhar652@gmail.com>
2022-05-13 16:13:11 +0530

    RECTIFIED PORT ERROR

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ sequelize
     console.log("SOMETHING WENT WRONG", err);
   });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
